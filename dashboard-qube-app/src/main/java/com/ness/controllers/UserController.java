4be3631b527a83eac82f7405936cda13901ad186
cristian.bucataru <cristian.bucataru.ext@porscheinformatik.com>
2022-05-02 13:26:35 +0300

    [VU3DEV-15656]: Changed response type to text/plain

--- dashboard-qube-app/src/main/java/com/ness/controllers/UserController.java
+++ dashboard-qube-app/src/main/java/com/ness/controllers/UserController.java
@@ -3,7 +3,10 @@ package com.ness.controllers;
 import com.ness.dtos.UserDTO;
 import com.ness.entities.User;
 import com.ness.services.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +21,14 @@ import java.util.Optional;
 public class UserController {
 
     private final UserServiceImpl userServiceImpl;
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserServiceImpl userServiceImpl) {
         this.userServiceImpl = userServiceImpl;
     }
 
     @CrossOrigin(origins = "*")
-    @PostMapping("/api/users")
+    @PostMapping(path="/api/users", produces=MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> save(@RequestBody UserDTO user){
         if(user.getEmail() == null || user.getDateOfBirth() == null ||
             user.getPassword() == null || user.getFirstName() == null ||
@@ -36,7 +40,6 @@ public class UserController {
         {
             System.out.println("User already exists");
             return ResponseEntity.status(409).body("User already exists");
-//            response.setStatus(409);
         }
         try{
             this.userServiceImpl.save(user);
@@ -44,7 +47,7 @@ public class UserController {
         }
         catch (Exception e)
         {
-            System.out.println(e);
+            logger.error(e.toString());
             return ResponseEntity.status(500).body("Internal server error");
         }
     }
